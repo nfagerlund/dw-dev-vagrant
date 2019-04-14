d3897059f2d961e2bcec4aeb0aad1b8e0c1c6f54
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-04-14 10:14:31 -0700

    notify IP addresses when provisioning finishes

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,1 +1,6 @@
 include "dw_dev"
+
+$my_ips = $facts['networking']['interfaces'].map |$name, $interface| {
+  $interface['ip']
+}.join(', ')
+notify {"Make sure to set up DNS so this machine's domain name (and all its subdomains) resolve to its bridged IP address.\n  Hostname: ${facts['fqdn']}\n  IP addresses: ${my_ips}.":}
